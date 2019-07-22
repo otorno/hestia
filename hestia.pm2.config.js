35d3ab4f0b2d173af3bcf467e663a4bd23f96ed7
Michael Fedora <mtc120@gmail.com>
2019-07-22 18:22:25 -0400

    fix pm2 config not using prod directory

--- hestia.pm2.config.js
+++ hestia.pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'hestia',
-      script: 'build/backend/hestia.js',
+      script: 'build-prod/backend/hestia.js',
       watch: true,
       env: {
         NODE_ENV: 'production'
